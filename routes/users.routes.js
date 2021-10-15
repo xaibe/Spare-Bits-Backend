dfe269ccc6c21d6b9f51b48129cbf56789c9765b
xaibe <sa30967965@gmail.com>
2021-10-15 08:26:23 +0500

    changed routes

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const UserController = require('../controllers/users.controllers');
-const checkAuth = require('../middleware/check-auth');
-const upload = require('../config/upload');
+const upload = require('../Config/upload');
 
 
 router.post("/uploadimage", upload.single('file'), UserController.uploadAvatar);
